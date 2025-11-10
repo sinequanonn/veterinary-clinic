fd837e40e6bd70803435cb2009264a16e6e8d2ac
sinequanonn <dleck288@gmail.com>
2025-11-10 22:30:30 +0900

    test: MedicalRecord 엔티티 추가

--- src/main/java/vet/vetclinic/domain/MedicalRecord.java
+++ src/main/java/vet/vetclinic/domain/MedicalRecord.java
@@ -8,6 +8,11 @@ import java.time.LocalDate;
 @Entity
 @Getter
 public class MedicalRecord {
+    public static final int MAX_SUBJECTIVE_LENGTH = 500;
+    public static final int MAX_OBJECTIVE_LENGTH = 500;
+    public static final int MAX_ASSESSMENT_LENGTH = 200;
+    public static final int MAX_PLAN_LENGTH = 500;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long medialRecordId;
@@ -31,10 +36,62 @@ public class MedicalRecord {
     }
 
     public MedicalRecord(LocalDate recordDate, String subjective, String objective, String assessment, String plan) {
+        validateRecordDate(recordDate);
+        validateSubjective(subjective);
+        validateObjective(objective);
+        validateAssessment(assessment);
+        validatePlan(plan);
+
         this.recordDate = recordDate;
         this.subjective = subjective;
         this.objective = objective;
         this.assessment = assessment;
         this.plan = plan;
+    }
+
+    private void validatePlan(String plan) {
+        if (plan == null || plan.isBlank()) {
+            throw new IllegalArgumentException("진료 계획은 1자 이상 500자 이하여야 합니다.");
+        }
+
+        if (plan.length() > MAX_PLAN_LENGTH) {
+            throw new IllegalArgumentException("진료 계획은 1자 이상 500자 이하여야 합니다.");
+        }
+    }
+
+    private void validateAssessment(String assessment) {
+        if (assessment == null || assessment.isBlank()) {
+            throw new IllegalArgumentException("확정 진단은 1자 이상 200자 이하여야 합니다.");
+        }
+
+        if (assessment.length() > MAX_ASSESSMENT_LENGTH) {
+            throw new IllegalArgumentException("확정 진단은 1자 이상 200자 이하여야 합니다.");
+        }
+    }
+
+    private void validateObjective(String objective) {
+        if (objective == null || objective.isBlank()) {
+            throw new IllegalArgumentException("객관적 판단은 1자 이상 500자 이하여야 합니다.");
+        }
+
+        if (objective.length() > MAX_OBJECTIVE_LENGTH) {
+            throw new IllegalArgumentException("객관적 판단은 1자 이상 500자 이하여야 합니다.");
+        }
+    }
+
+    private void validateSubjective(String subjective) {
+        if (subjective == null || subjective.isBlank()) {
+            throw new IllegalArgumentException("주관적 판단은 1자 이상 500자 이하여야 합니다.");
+        }
+
+        if (subjective.length() > MAX_SUBJECTIVE_LENGTH) {
+            throw new IllegalArgumentException("주관적 판단은 1자 이상 500자 이하여야 합니다.");
+        }
+    }
+
+    private void validateRecordDate(LocalDate recordDate) {
+        if (recordDate.isAfter(LocalDate.now())) {
+            throw new IllegalArgumentException("날짜는 오늘 이전 날짜여야 합니다.");
+        }
     }
 }
