6912322d13477f287435de2f2e308aa9c368d814
sinequanonn <dleck288@gmail.com>
2025-11-10 20:23:11 +0900

    test: Vet 생성 테스트 작성
    - 이름으로 수의사 생성
    - 수의사명 길이가 20일 때 생성
    - 수의사명이 null이거나 공백이면 예외 발생
    - 수의사명 길이가 20 초과이면 예외 발생

--- src/test/java/vet/vetclinic/domain/VetTest.java
+++ src/test/java/vet/vetclinic/domain/VetTest.java
@@ -2,6 +2,12 @@ package vet.vetclinic.domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class VetTest {
     @Test
@@ -13,6 +19,40 @@ public class VetTest {
         Vet vet = new Vet("박진우");
 
         //then
-        Assertions.assertThat(vet.getVetName()).isEqualTo(vetName);
+        assertThat(vet.getVetName()).isEqualTo(vetName);
+    }
+
+    @Test
+    void 수의사명_길이가_정확히_20이면_수의사를_생성한다() {
+        //given
+        String vetName = "a".repeat(20);
+
+        //when
+        Vet vet = new Vet(vetName);
+
+        //then
+        assertThat(vet.getVetName()).isEqualTo(vetName);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "  ", "\t", "\n"})
+    void 수의사명이_null이거나_공백이면_예외가_발생한다(String vetName) {
+        //when&then
+        assertThatThrownBy(() -> new Vet(vetName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("수의사명은 필수 입력값입니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {21, 100, 150})
+    void 수의사명이_20자를_초과하면_예외가_발생한다(int repetition) {
+        //given
+        String vetName = "a".repeat(repetition);
+
+        //when&then
+        assertThatThrownBy(() -> new Vet(vetName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("수의사명은 1자 이상 20자 이하여야 합니다.");
     }
 }
