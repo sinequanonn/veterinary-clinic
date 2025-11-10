c7b315f50a9cc54b3225661005780468d6933425
sinequanonn <dleck288@gmail.com>
2025-11-10 21:48:43 +0900

    feat: Pet 체중과 생년월일 유효성 검증 테스트 작성
    - 체중은 0이하이면 예외발생
    - 생년월일은 미래 날짜이면 예외발생

--- src/test/java/vet/vetclinic/domain/PetTest.java
+++ src/test/java/vet/vetclinic/domain/PetTest.java
@@ -127,4 +127,34 @@ public class PetTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("품종은 1자 이상 20자 이하여야 합니다.");
     }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {0, -1.0, -5.5})
+    void 체중이_0이하이면_예외가_발생한다(double weight) {
+        //given
+        String petName = "뽀삐";
+        String ownerName = "박진우";
+        String breed = "말티즈";
+        LocalDate birthDate = LocalDate.of(2025, 11, 8);
+
+        //when
+        assertThatThrownBy(() -> new Pet(petName, ownerName, breed, weight, birthDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("체중은 0보다 커야 합니다.");
+    }
+
+    @Test
+    void 생년월일이_미래_날짜이면_예외가_발생한다() {
+        //given
+        String petName = "뽀삐";
+        String ownerName = "박진우";
+        String breed = "말티즈";
+        double weight = 3.5;
+        LocalDate birthDate = LocalDate.now().plusDays(2);
+
+        //when
+        assertThatThrownBy(() -> new Pet(petName, ownerName, breed, weight, birthDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("날짜는 오늘 이전 날짜여야 합니다.");
+    }
 }
