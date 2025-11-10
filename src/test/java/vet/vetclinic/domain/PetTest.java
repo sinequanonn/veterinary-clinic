49fba0a8bcee771f5d799d231098a791f5f929cf
sinequanonn <dleck288@gmail.com>
2025-11-10 21:35:10 +0900

    test: Pet 보호자명 품종 유효성 검증 구현
    - 보호자명 품종은 1자 이상 20자 이하

--- src/test/java/vet/vetclinic/domain/PetTest.java
+++ src/test/java/vet/vetclinic/domain/PetTest.java
@@ -63,4 +63,68 @@ public class PetTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("환자 이름은 1자 이상 20자 이하여야 합니다.");
     }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "  ", "\t", "\n"})
+    void 보호자명이_null이거나_공백이면_예외를_발생한다(String ownerName) {
+        //given
+        String petName = "뽀삐";
+        String breed = "말티즈";
+        double weight = 3.5;
+        LocalDate birthDate = LocalDate.of(2025, 11, 8);
+
+        //when
+        assertThatThrownBy(() -> new Pet(petName, ownerName, breed, weight, birthDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("보호자명은 1자 이상 20자 이하여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {21, 50, 101})
+    void 보호자명이_20자_초과하면_예외를_발생한다(int repetition) {
+        //given
+        String petName = "뽀삐";
+        String ownerName = "a".repeat(repetition);
+        String breed = "말티즈";
+        double weight = 3.5;
+        LocalDate birthDate = LocalDate.of(2025, 11, 8);
+
+        //when
+        assertThatThrownBy(() -> new Pet(petName, ownerName, breed, weight, birthDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("보호자명은 1자 이상 20자 이하여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "  ", "\t", "\n"})
+    void 품종이_null이거나_공백이면_예외를_발생한다(String breed) {
+        //given
+        String petName = "뽀삐";
+        String ownerName = "박진우";
+        double weight = 3.5;
+        LocalDate birthDate = LocalDate.of(2025, 11, 8);
+
+        //when
+        assertThatThrownBy(() -> new Pet(petName, ownerName, breed, weight, birthDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("품종은 1자 이상 20자 이하여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {21, 50, 101})
+    void 품종이_20자_초과하면_예외를_발생한다(int repetition) {
+        //given
+        String petName = "뽀삐";
+        String ownerName = "박진우";
+        String breed = "a".repeat(repetition);
+        double weight = 3.5;
+        LocalDate birthDate = LocalDate.of(2025, 11, 8);
+
+        //when
+        assertThatThrownBy(() -> new Pet(petName, ownerName, breed, weight, birthDate))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("품종은 1자 이상 20자 이하여야 합니다.");
+    }
 }
