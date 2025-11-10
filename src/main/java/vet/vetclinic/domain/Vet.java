c0270c4a3f6129aafb3f7d4d38f3a85a89487d85
sinequanonn <dleck288@gmail.com>
2025-11-10 20:24:20 +0900

    feat: Vet 도메인 엔티티 구현
    - 수의사명 유효성 검증 로직 추가

--- src/main/java/vet/vetclinic/domain/Vet.java
+++ src/main/java/vet/vetclinic/domain/Vet.java
@@ -6,17 +6,31 @@ import lombok.Getter;
 @Getter
 @Entity
 public class Vet {
+    private static final int MAX_NAME_LENGTH = 20;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long vetId;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length=MAX_NAME_LENGTH)
     private String vetName;
 
     protected Vet() {
     }
 
     public Vet(String vetName) {
+        validateVetName(vetName);
         this.vetName = vetName;
+    }
+
+    private void validateVetName(String vetName) {
+        if (vetName == null || vetName.isBlank()) {
+            throw new IllegalArgumentException("수의사명은 필수 입력값입니다.");
+        }
+
+
+        if (vetName.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("수의사명은 1자 이상 20자 이하여야 합니다.");
+        }
     }
 }
