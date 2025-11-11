5a9cbd4b69c30197acedc217d71e1074a7b15135
sinequanonn <dleck288@gmail.com>
2025-11-11 22:40:11 +0900

    feat: findByPet_PetId 추가

--- src/main/java/vet/vetclinic/repository/MedicalReportRepository.java
+++ src/main/java/vet/vetclinic/repository/MedicalReportRepository.java
@@ -3,5 +3,8 @@ package vet.vetclinic.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import vet.vetclinic.domain.MedicalReport;
 
+import java.util.List;
+
 public interface MedicalReportRepository extends JpaRepository<MedicalReport, Long> {
+    List<MedicalReport> findByPet_PetId(Long petPetId);
 }
