0fbc27edf42d10f152cfcf4e7cfc6f8872edeee0
sinequanonn <dleck288@gmail.com>
2025-11-11 20:22:14 +0900

    test: VetRepository 모든 수의사 조회 텍스트 작성

--- src/test/java/vet/vetclinic/repository/VetRepositoryTest.java
+++ src/test/java/vet/vetclinic/repository/VetRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import vet.vetclinic.domain.Vet;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -24,5 +26,22 @@ public class VetRepositoryTest {
         //then
         assertThat(savedVet.getVetId()).isNotNull();
         assertThat(savedVet.getVetName()).isEqualTo("박진우");
+    }
+
+    @Test
+    void 모든_수의사를_조회한다() {
+        //given
+        vetRepository.save(new Vet("박진우"));
+        vetRepository.save(new Vet("박진운"));
+        vetRepository.save(new Vet("김철수"));
+
+        //when
+        List<Vet> vets = vetRepository.findAll();
+
+        //then
+        assertThat(vets).hasSize(3);
+        assertThat(vets)
+                .extracting("vetName")
+                .containsExactly("박진우", "박진운", "김철수");
     }
 }
