d58e3c37d844e6090d75000b520e9657288f25fb
SineQunNon <dleck28@gmail.com>
2025-11-12 21:23:44 +0900

    feat: 수의사 DTO 추가

---
+++ src/main/java/vet/vetclinic/dto/VetRequest.java
@@ -0,0 +1,12 @@
+package vet.vetclinic.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VetRequest {
+    private String vetName;
+}
---
+++ src/main/java/vet/vetclinic/dto/VetResponse.java
@@ -0,0 +1,19 @@
+package vet.vetclinic.dto;
+
+import lombok.Getter;
+import vet.vetclinic.domain.Vet;
+
+@Getter
+public class VetResponse {
+    private Long vetId;
+    private String vetName;
+
+    private VetResponse(Long vetId, String vetName) {
+        this.vetId = vetId;
+        this.vetName = vetName;
+    }
+
+    public static VetResponse from(Vet vet) {
+        return new VetResponse(vet.getVetId(), vet.getVetName());
+    }
+}
