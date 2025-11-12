68bb4e6d932d2238dbd6dc6284f22a63b2281eea
SineQunNon <dleck28@gmail.com>
2025-11-12 22:10:12 +0900

    feat: 환자 목록 조회 API 구현

--- src/main/java/vet/vetclinic/controller/PetController.java
+++ src/main/java/vet/vetclinic/controller/PetController.java
@@ -2,14 +2,14 @@ package vet.vetclinic.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import vet.vetclinic.domain.Pet;
 import vet.vetclinic.dto.PetRequest;
 import vet.vetclinic.dto.PetResponse;
 import vet.vetclinic.service.PetService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/pet")
@@ -31,5 +31,13 @@ public class PetController {
         );
         PetResponse response = PetResponse.from(pet);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PetResponse>> findAll() {
+        List<PetResponse> responses = petService.findAll().stream()
+                .map(PetResponse::from)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(responses);
     }
 }
