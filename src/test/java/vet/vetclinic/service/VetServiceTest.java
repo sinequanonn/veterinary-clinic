2dc60bd0a9d61ae1c17efba1b3c7db69e3a242ec
sinequanonn <dleck288@gmail.com>
2025-11-12 12:50:11 +0900

    test: VetService 모든 수의사 목록 조회 테스트 작성

--- src/test/java/vet/vetclinic/service/VetServiceTest.java
+++ src/test/java/vet/vetclinic/service/VetServiceTest.java
@@ -9,6 +9,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import vet.vetclinic.domain.Vet;
 import vet.vetclinic.repository.VetRepository;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -34,5 +36,24 @@ public class VetServiceTest {
         //then
         assertThat(registered.getVetName()).isEqualTo(vetName);
         verify(vetRepository, times(1)).save(any(Vet.class));
+    }
+
+    @Test
+    void 모든_수의사의_목록을_조회한다() {
+        //given
+        List<Vet> vets = List.of(
+                new Vet("박진우"),
+                new Vet("박지운")
+        );
+        when(vetRepository.findAll()).thenReturn(vets);
+
+        //when
+        List<Vet> foundVets = vetService.findAll();
+
+        //then
+        assertThat(foundVets).hasSize(2);
+        assertThat(foundVets).extracting("vetName")
+                .containsExactly("박진우", "박지운");
+        verify(vetRepository, times(1)).findAll();
     }
 }
