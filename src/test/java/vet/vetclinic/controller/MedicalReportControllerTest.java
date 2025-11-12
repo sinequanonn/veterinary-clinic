7614b419167115658d110fabc603a2dacd3f1fdd
SineQunNon <dleck28@gmail.com>
2025-11-13 02:11:00 +0900

    test: 환자 진료 소견서 목록 조회 테스트 작성

--- src/test/java/vet/vetclinic/controller/MedicalReportControllerTest.java
+++ src/test/java/vet/vetclinic/controller/MedicalReportControllerTest.java
@@ -12,11 +12,14 @@ import org.springframework.transaction.annotation.Transactional;
 import vet.vetclinic.domain.Pet;
 import vet.vetclinic.domain.Vet;
 import vet.vetclinic.dto.MedicalReportRequest;
+import vet.vetclinic.service.MedicalReportService;
 import vet.vetclinic.service.PetService;
 import vet.vetclinic.service.VetService;
 
 import java.time.LocalDate;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,6 +32,7 @@ public class MedicalReportControllerTest {
     @Autowired private ObjectMapper objectMapper;
     @Autowired private VetService vetService;
     @Autowired private PetService petService;
+    @Autowired private MedicalReportService medicalReportService;
 
     private Pet pet;
     private Vet vet;
@@ -67,5 +71,21 @@ public class MedicalReportControllerTest {
                 .andExpect(jsonPath("$.assessment").value("식도염"))
                 .andExpect(jsonPath("$.plan").value("수액처치 및 약물 투여"))
                 .andExpect(jsonPath("$.postoperativeCare").value("3일간 금식, 물만 제공"));
+    }
+
+    @Test
+    void 환자의_진료소견서_목록을_조회한다() throws Exception {
+        //given
+        medicalReportService.create(pet.getPetId(), vet.getVetId(), LocalDate.of(2025, 11, 9), "식욕부진, 구토", "식도염", "수액처치 및 약물 투여", "3일간 금식, 물만 제공");
+        medicalReportService.create(pet.getPetId(), vet.getVetId(), LocalDate.of(2025, 11, 10), "식욕부진, 구토", "식도염", "수액처치 및 약물 투여", "3일간 금식, 물만 제공");
+        medicalReportService.create(pet.getPetId(), vet.getVetId(), LocalDate.of(2025, 11, 11), "식욕부진, 구토", "식도염", "수액처치 및 약물 투여", "3일간 금식, 물만 제공");
+
+        //when&then
+        mockMvc.perform(get("/api/v1/medical-reports/pets/{petId}", pet.getPetId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].reportDate").value("2025-11-09"))
+                .andExpect(jsonPath("$[1].reportDate").value("2025-11-10"))
+                .andExpect(jsonPath("$[2].reportDate").value("2025-11-11"));
     }
 }
