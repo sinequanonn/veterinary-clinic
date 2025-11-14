06c98b82699ca752d64fdda9d12655eb59d94575
sinequanonn <dleck288@gmail.com>
2025-11-14 13:24:45 +0900

    refactor: 수의사 등록 Dto 리팩토링
    - 직관적인 이름으로 변경 (VetRequest -> VetCreateRequest)
    - Builder 애노테이션 추가
    - 정적 팩토리 메서드 추가
    - 필드 검증 추가

---
+++ src/main/java/vet/vetclinic/dto/request/VetCreateRequest.java
@@ -0,0 +1,25 @@
+package vet.vetclinic.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import vet.vetclinic.domain.Vet;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class VetCreateRequest {
+    @NotBlank(message = "수의사명은 필수 입력값입니다.")
+    @Size(min = 1, max = 20, message = "수의사명은 1자 이상 20자 이하여야 합니다.")
+    private String vetName;
+
+    @Builder
+    public VetCreateRequest(String vetName) {
+        this.vetName = vetName;
+    }
+
+    public Vet toEntity() {
+        return Vet.builder()
+                .vetName(vetName)
+                .build();
+    }
+}
