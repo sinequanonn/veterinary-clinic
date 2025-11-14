bf87cb11603f7165fc5b53c4e5c96e0efafe8bde
sinequanonn <dleck288@gmail.com>
2025-11-14 20:57:54 +0900

    refactor: 환자 등록 Dto 리팩토링
    - 필드 검증 추가
    - 직관적인 이름으로 변경
    - 엔티티 생성 로직 위임
    - 빌더 추가

---
+++ src/main/java/vet/vetclinic/dto/request/PetCreateRequest.java
@@ -0,0 +1,50 @@
+package vet.vetclinic.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+import vet.vetclinic.domain.Pet;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PetCreateRequest {
+    @NotBlank
+    @Size(min=1, max=20, message="환자 이름은 1자 이상 20자 이하여야 합니다.")
+    private String petName;
+
+    @NotBlank
+    @Size(min=1, max=20, message="보호자명은 1자 이상 20자 이하여야 합니다.")
+    private String ownerName;
+
+    @NotBlank
+    @Size(min=1, max=20, message="품종은 1자 이상 20자 이하여야 합니다.")
+    private String breed;
+
+    @NotNull
+    @Positive(message = "체중은 0보다 커야 합니다.")
+    private double weight;
+
+    @NotNull
+    @PastOrPresent(message = "날짜는 오늘 이전 날짜여야 합니다.")
+    private LocalDate birthDate;
+
+    @Builder
+    public PetCreateRequest(String petName, String ownerName, String breed, double weight, LocalDate birthDate) {
+        this.petName = petName;
+        this.ownerName = ownerName;
+        this.breed = breed;
+        this.weight = weight;
+        this.birthDate = birthDate;
+    }
+
+    public Pet toEntity() {
+        return Pet.builder()
+                .petName(petName)
+                .ownerName(ownerName)
+                .breed(breed)
+                .weight(weight)
+                .birthDate(birthDate)
+                .build();
+    }
+}
