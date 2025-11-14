349e1c7edce6beb3a959d954265377857f3a7497
sinequanonn <dleck288@gmail.com>
2025-11-14 14:00:02 +0900

    refactor: VetService 리팩토링
    - 서비스가 Request Dto를 받아서 Reponse Dto를 반환하도록 로직 변경
    - findAll 수의사 목록을 생성하도록 수정

--- src/main/java/vet/vetclinic/service/VetService.java
+++ src/main/java/vet/vetclinic/service/VetService.java
@@ -3,6 +3,8 @@ package vet.vetclinic.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import vet.vetclinic.domain.Vet;
+import vet.vetclinic.dto.request.VetCreateRequest;
+import vet.vetclinic.dto.response.VetResponse;
 import vet.vetclinic.repository.VetRepository;
 
 import java.util.List;
@@ -17,17 +19,20 @@ public class VetService {
     }
 
     @Transactional
-    public Vet register(String vetName) {
-        Vet vet = new Vet(vetName);
-        return vetRepository.save(vet);
+    public VetResponse createVet(VetCreateRequest request) {
+        return VetResponse.from(vetRepository.save(request.toEntity()));
     }
 
-    public Vet findById(Long vetId) {
-        return vetRepository.findById(vetId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 수의사입니다."));
+    public VetResponse findById(Long vetId) {
+        return vetRepository.findById(vetId)
+                .map(VetResponse::from)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 수의사입니다."));
     }
 
-    public List<Vet> findAll() {
-        return vetRepository.findAll();
+    public List<VetResponse> findAll() {
+        return vetRepository.findAll().stream()
+                .map(VetResponse::from)
+                .toList();
     }
 
     @Transactional
