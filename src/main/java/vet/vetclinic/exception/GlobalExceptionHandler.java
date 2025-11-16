1c824ed290e1b54994bec71cd2dc8a17c3a04704
sinequanonn <dleck288@gmail.com>
2025-11-16 20:21:56 +0900

    feat: 커스텀 Exception 처리 핸들러 구현

--- src/main/java/vet/vetclinic/exception/GlobalExceptionHandler.java
+++ src/main/java/vet/vetclinic/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import vet.vetclinic.dto.response.ErrorResponse;
 import vet.vetclinic.dto.response.ValidationErrorResponse;
 import vet.vetclinic.dto.response.ValidationErrorResponse.FieldError;
 
@@ -22,4 +23,16 @@ public class GlobalExceptionHandler {
         ValidationErrorResponse response = new ValidationErrorResponse(errors);
         return ResponseEntity.badRequest().body(response);
     }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException exception) {
+        ErrorResponse response = ErrorResponse.of(
+                exception.getErrorCode().name(),
+                exception.getErrorCode().getMessage());
+
+        return ResponseEntity
+                .status(exception.getErrorCode().getStatus())
+                .body(response);
+    }
+
 }
